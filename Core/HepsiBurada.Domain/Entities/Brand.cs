b7180f45542611e4f4264b772e98a78f6081de56
ismailcagan <ismailcagan95@gmail.com>
2024-10-25 21:19:02 +0300

    AppsettingDüzenlendi

---
+++ Core/HepsiBurada.Domain/Entities/Brand.cs
@@ -0,0 +1,18 @@
+﻿using HepsiBurada.Domain.Common;
+
+namespace HepsiBurada.Domain.Entities
+{
+    // Markalar
+    public class Brand : EntityBase
+    {
+        public Brand()
+        {
+            
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public required string Name { get; set; }
+    }
+}
---
+++ Core/HepsiBurada.Domain/Entities/Category.cs
@@ -0,0 +1,23 @@
+﻿using HepsiBurada.Domain.Common;
+
+namespace HepsiBurada.Domain.Entities
+{
+    public class Category : EntityBase
+    {
+        public Category()
+        {
+            
+        }
+        public Category(int parentId, string name, int priorty)
+        {
+            ParentId = parentId;
+            Name = name;
+            Priorty = priorty;
+        }
+        public required int ParentId { get; set; }
+        public required string Name { get; set; }
+        public required int Priorty { get; set; }
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
