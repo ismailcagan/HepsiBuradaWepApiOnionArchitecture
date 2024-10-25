d4d21fcdea03a716ee6abdd5ab0deadba23abeef
ismailcagan <ismailcagan95@gmail.com>
2024-10-25 23:27:06 +0300

    Temel Entityler Oluşturuldu

---
+++ Core/HepsiBurada.Domain/Entities/Brand.cs
@@ -0,0 +1,19 @@
+﻿using HepsiBurada.Domain.Common;
+
+namespace HepsiBurada.Domain.Entities
+{
+    // Markalar
+    public class Brand : EntityBase
+    {
+        public Brand()
+        {
+            
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+
+        public required string Name {  get; set; }
+    }
+}
